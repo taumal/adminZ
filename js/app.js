102dee38ed6ce6d5706b01f58e48c7f7edf4dcb4
MD. Mafijur Rahman Taumal <taumal@quantummethod.org.bd>
2018-09-09 20:34:31 +0600

    First Commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).foundation();
+
+$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+  e.preventDefault();
+  $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+});
+
+// more click
+$('.card-profile-stats-more-link').click(function(e){
+  e.preventDefault();
+  if ( $(".card-profile-stats-more-content").is(':hidden') ) {
+    $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+  } else {
+    $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+  }
+  $(this).next('.card-profile-stats-more-content').slideToggle();
+});
+
